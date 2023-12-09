7ddec4a288221033821a234a406e6a70ac5eba68
realOskarA <153332019+realOskarA@users.noreply.github.com>
2023-12-09 10:25:50 +0100

    day9

---
+++ day9.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass
+from copy import deepcopy
+from typing import Tuple
+
+
+def parse_input() -> list[list[int]]:
+
+    readings: list[list[int]] = []
+
+    with open("day9input.txt", "r") as f:
+        lines = f.readlines()
+        for l in lines:
+            s_numbs = l.strip().split(" ")
+            numbs = list(map(lambda s: int(s), s_numbs))
+            readings.append(numbs)
+
+    return readings
+
+
+def predict_sequence(numbs: list[int]) -> int:
+
+    steps: list[list[int]] = []
+
+    steps.append(numbs)
+
+    step_index = 0
+    while True:
+        new_steps: list[int] = []
+        for i in range(0, len(steps[step_index])-1):
+            new_steps.append(steps[step_index][i+1] - steps[step_index][i])
+
+        steps.append(new_steps)
+        step_index += 1
+
+        step_sum = 0
+        for s in steps[step_index]:
+            step_sum += s
+
+        if step_sum == 0:
+            break
+
+    new_num = 0
+    for i in range(0, len(steps)):
+        new_num += steps[i][-1]
+
+    print(new_num)
+    return new_num
+
+
+if __name__ == "__main__":
+    readings = parse_input()
+
+    result = 0
+    for r in readings:
+        result += predict_sequence(r)
+
+    print(result)
